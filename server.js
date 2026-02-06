87b33768312e462b1c0974fe2bc61dc03cff1655
Krishika.B <krishikabalamurali@gmail.com>
2026-02-06 21:22:40 +0530

    backend ready

--- server.js
+++ server.js
@@ -137,3 +137,6 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.listen(5000, () => console.log('Server running on port 5000')); 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
